ed64db522d47d99b816e6c21f251bbd110e7566a
francovinicius <duartevini.dev@gmail.com>
2023-08-23 16:15:45 -0300

    finalizando a implementação do adapter

--- app/src/main/java/com/vinicius/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/vinicius/orgs/ui/activity/MainActivity.kt
@@ -2,9 +2,12 @@ package com.vinicius.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.vinicius.orgs.R
+import com.vinicius.orgs.model.Produto
 import com.vinicius.orgs.ui.recyclerview.adapter.ListaProdutoAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -24,7 +27,21 @@ class MainActivity : Activity() {
          */
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.adapter = ListaProdutoAdapter()
+        recyclerView.adapter = ListaProdutoAdapter(
+            context = this, produtos = listOf(
+                Produto(
+                    nome = "testye",
+                    descricao = "test",
+                    preco = BigDecimal("19.99")
+                ),
+                Produto(
+                    nome = "teste",
+                    descricao = "test1",
+                    preco = BigDecimal("89.99")
+                ),
+            )
+        )
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
     }
--- app/src/main/java/com/vinicius/orgs/ui/recyclerview/adapter/ListaProdutoAdapter.kt
+++ app/src/main/java/com/vinicius/orgs/ui/recyclerview/adapter/ListaProdutoAdapter.kt
@@ -1,19 +1,41 @@
 package com.vinicius.orgs.ui.recyclerview.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.vinicius.orgs.R
 import com.vinicius.orgs.model.Produto
 
-class ListaProdutoAdapter (
+class ListaProdutoAdapter(
+    private val context: Context,
     private val produtos: List<Produto>
 
-)    : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+)    : RecyclerView.Adapter<ListaProdutoAdapter.ViewHolder>() {
+
+    class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+        fun vincula(produto: Produto) {
+            val nome = itemView.findViewById<TextView>(R.id.nome)
+            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            val preco = itemView.findViewById<TextView>(R.id.preco)
+            nome.text = produto.nome
+            descricao.text = produto.descricao
+            preco.text = produto.preco.toPlainString()
+        }
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val produto = produtos[position]
+        holder.vincula(produto)
     }
 
     override fun getItemCount(): Int = produtos.size
