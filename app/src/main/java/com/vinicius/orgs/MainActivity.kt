2193e72760eecd79840f9c159ed4c55278a766aa
francovinicius <duartevini.dev@gmail.com>
2023-08-23 10:03:39 -0300

    Add layout

--- app/src/main/java/com/vinicius/orgs/MainActivity.kt
+++ app/src/main/java/com/vinicius/orgs/MainActivity.kt
@@ -3,13 +3,17 @@ package com.vinicius.orgs
 import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Bem vindo(a) ao Orgs", Toast.LENGTH_SHORT).show()
+        val view = TextView(this)
+        view.setText("Cesta de frutas")
+        setContentView(view)
     }
 
 }
